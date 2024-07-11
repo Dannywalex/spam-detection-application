a76de9c83557e7ce8f5e6b420e5c68c97fb8dee9
Dannywalex <akinbusoyekayode.com@gmail.com>
2024-07-11 17:22:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -54,8 +54,13 @@ if authorization_response:
     try:
         zoho = OAuth2Session(client_id, redirect_uri=redirect_uri, state=st.session_state['oauth_state'])
 
+        # Extract the authorization code from the response URL
+        authorization_code = authorization_response.split('code=')[1].split('&')[0]
+        st.write(f"Authorization Code: {authorization_code}")
+
         token = zoho.fetch_token(
             'https://accounts.zoho.com/oauth/v2/token',
+            code=authorization_code,
             client_secret=client_secret,
             include_client_id=True
         )
