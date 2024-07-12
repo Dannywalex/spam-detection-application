e1357b0f30a2b5b858dc71234565aaf034b03811
Dannywalex <akinbusoyekayode.com@gmail.com>
2024-07-12 13:11:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,29 @@ import pickle
 import time
 from requests_oauthlib import OAuth2Session
 
+
+feature_extraction = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+st.title("Email Spam Detection")
+input_email = st.text_input("Enter the message")
+
+if st.button('Predict'):
+
+
+
+  #1. preprocess
+  input_mail = ["Ok lar... Joking wif u oni..."]
+  #2.vectorize
+  input_data_feature = feature_extraction.transform([input_email])
+  #3.predict
+  prediction = model.predict(input_data_feature)
+  #4.Display
+  if prediction[0] == 1:
+   st.header("Not Spam")
+  else:
+   st.header("Spam")
+
+
 # Load the token
 try:
     with open('token.pkl', 'rb') as token_file:
